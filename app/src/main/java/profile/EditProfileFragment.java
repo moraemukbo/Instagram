8aea4d943128822edc4de3c8c4b92392f148059c
moraemukbo <96509106+moraemukbo@users.noreply.github.com>
2022-02-21 14:16:42 +0900

    ..

---
+++ app/src/main/java/profile/EditProfileFragment.java
@@ -0,0 +1,20 @@
+package profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.techtown.instagram.R;
+
+public class EditProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_home,container,false);
+    }
+}
