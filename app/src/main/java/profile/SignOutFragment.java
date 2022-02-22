3159f1dc93ac696fd7a3f5dccf6d4748f01c92d3
moraemukbo <96509106+moraemukbo@users.noreply.github.com>
2022-02-22 23:12:23 +0900

    ..

--- app/src/main/java/profile/SignOutFragment.java
+++ app/src/main/java/profile/SignOutFragment.java
@@ -11,11 +11,11 @@ import androidx.fragment.app.Fragment;
 
 import org.techtown.instagram.R;
 
-public class EditProfileFragment extends Fragment {
+public class SignOutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_editprofile,container,false);
+        View view =inflater.inflate(R.layout.fragment_signout,container,false);
         return view;
     }
 }
